9952ad40ab4a3603e6512bf69082fb623ff1fe96
bilelkort <bilel.kort@esprit.tn>
2023-03-25 16:20:56 +0100

    first commit

---
+++ facebook-scraper-master/test.py
@@ -0,0 +1,31 @@
+import facebook_scraper as fs
+
+# get POST_ID from the URL of the post which can have the following structure:
+# https://www.facebook.com/USER/posts/POST_ID
+# https://www.facebook.com/groups/GROUP_ID/posts/POST_ID
+POST_ID = "pfbid09hLyY29YvnYthrovH7E8Zn9QnrDKGU9GQoFXnEnD3A2zZ6yjoKavYQLR3yzAQnuZl"
+
+# number of comments to download -- set this to True to download all comments
+MAX_COMMENTS = 100
+
+# get the post (this gives a generator)
+gen = fs.get_posts(
+    post_urls=[POST_ID],
+    options={"comments": MAX_COMMENTS, "progress": True}
+)
+
+# take 1st element of the generator which is the post we requested
+post = next(gen)
+
+# extract the comments part
+comments = post['comments_full']
+
+# process comments as you want...
+for comment in comments:
+
+    # e.g. ...print them
+    print(comment)
+
+    # e.g. ...get the replies for them
+    for reply in comment['replies']:
+        print(' ', reply)
